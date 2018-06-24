e6f07c26a1240801f42f76da1487e6ac8e125bda
thegodfather <AliFattahiproject@gmail.com>
2018-06-24 21:06:39 +0430

    add connect handler

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 var TAG = '  **-----------server.js : ';
 
 const  path = require('path');
+const http = require('http');
 const  express = require('express');
+const socketIo = require('socket.io');
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIo(server);
 
 app.use(express.static(publicPath));
 
+   io.on('connection',(socket)=>{
+      console.log('New USer Connected');
+   });
 
-app.listen(port,()=>{
+server.listen(port,()=>{
    console.log(`Server is up on port ${port}`);
 });
